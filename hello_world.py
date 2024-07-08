12e1d9c9220cbcc3d637fb3f57586596df9a5c4f
p1v2 <pavliuk96@gmail.com>
2024-07-08 20:06:44 +0300

    add clock

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,12 @@
+from datetime import datetime
+
+
 def hello():
     print("initialization")
     print("Hello world!")
+    print(datetime.now())
     print("Goodbye")
+
+
+if __name__ == "__main__":
+    hello()
