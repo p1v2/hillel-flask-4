dd0574af158e85c655aa370e6bdc311ba6b3e58a
p1v2 <pavliuk96@gmail.com>
2024-07-08 20:33:07 +0300

    Revert "Add operation system"
    This reverts commit 614b90bc

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-import platform
 
 
 def hello():
     print("initialization")
     print("Hello world!")
     print(datetime.now())
-    # Print operation system version
-    print(platform.platform())
     # print day of week
     print(datetime.now().strftime("%A"))
     print("Goodbye")
