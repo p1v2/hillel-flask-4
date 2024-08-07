908c1324060d7c3470861b17b2593ec27e1f9e2d
ivertepny <ivertepny@gmail.com>
2024-08-07 10:50:15 +0300

    Corrected status code, added code 404

--- tests/test_app.py
+++ tests/test_app.py
@@ -71,7 +71,7 @@ class AppTestCase(unittest.TestCase):
     def test_product_delete(self):
         response = self.app.delete("/products", json={"name": "Product_for_Deleting", "price": 150})
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 204)
         self.assertEqual(response.json["message"], "Product deleted successfully")
 
         # Verify if the product is deleted
